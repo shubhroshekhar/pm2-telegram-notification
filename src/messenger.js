4bc15b277a1934ab1947f3618e00a283f0f5e1a5
Navin Tellis <navintellis@gmail.com>
2021-02-10 14:29:49 +0530

    appender and formater implemented

--- src/messenger.js
+++ src/messenger.js
@@ -8,7 +8,7 @@ const send_message_to_telegram = (botId, chatId, message) => {
   console.log(message);
   console.log('----------------------------------------\n');
   if ( botId && chatId && message) {
-    const url = `https://api.telegram.org/bot${botId}/sendMessage?chat_id=-${chatId}&text=${'navin:'+encodeURI(message)}&parse_mode=html`
+    const url = `https://api.telegram.org/bot${botId}/sendMessage?chat_id=-${chatId}&text=${encodeURI(message)}&parse_mode=html`
     const req = https.request(url, res => {
       res.on('data', () => {
         console.log("Message Sent");
@@ -29,77 +29,92 @@ const messagingModes = {
   collecting: 'COLLECTING',
 } 
 
+
+
 function DebounceEngine (config) {
 
   const self=this;
-  let debIntHandler;
-  const debBucket =[];
-  const eventTraffic={
-    lastMessageTimeStamp:null,
-    consistentViolationCount:0,
+  let deb_int_handler;
+  const deb_bucket =[];
+  const event_traffic={
+    last_message_time_stamp:null,
+    consistent_violation_count:0,
   }
 
-  const violationTime=config.violationTime || config.violationTime !==0 ?   5000: config.violationTime;
-  const violationCount=config.violationCount || config.violationCount !==0 ?   8: config.violationCount;
-  const debounceTime=config.debounce || config.debounce !==0 ?   30000: config.debounce;
-
+  const violation_time=config.violation_time || config.violation_time !==0 ? 5000: config.violation_time;
+  const violation_count=config.violation_count || config.violation_count !==0 ? 8: config.violation_count;
+  const debounce_time=config.debounce || config.debounce !==0 ? 30000: config.debounce;
+  const telegram_message_length_limit = config.telegram_message_length_limit || config.telegram_message_length_limit !==0 ? 1900: config.telegram_message_length_limit;
 
 
   const init = () => {
-    self.startUp();
+    self.start_up();
   }
-  const flushDebBucket = () => {
-    const debBucketQueueCount = debBucket.length;
+  const flush_deb_bucket = () => {
+    const deb_bucket_queue_count = deb_bucket.length;
 
-    if(debBucketQueueCount){
+    if(deb_bucket_queue_count){
       console.log('Flushing DebounceBucket Queue');
-      while(debBucket.length){
-        const msgNode=debBucket.shift();
-        send_message_to_telegram(config.bot_token, config.chat_id, msgNode.msg)
-      }
+      append_format_send_message();
     } else {
-      if(debIntHandler){
+      if(deb_int_handler){
         console.log('Nothing to Flush as DebounceEngine Queue is empty, Shutting down Engine')
-        self.shutDown();
+        self.shut_down();
       }
     }
   };
-
-  self.addToQueue=(msg) => {
-    if(!debIntHandler){
-      console.log('DebounceEngine is not running, invoking startup to get it up and running');
-      self.startUp()
+  const append_format_send_message= () => {
+    let message_to_be_sent='';
+    while(deb_bucket.length){
+      const msg_node=deb_bucket.shift();
+      const countSuffix=msg_node.count? `(${msg_node.count})`:'';
+      const formatted_message=`${msg_node.timestamp} | ${msg_node.msg}\n${countSuffix}\n\n`
+      if((message_to_be_sent.length+formatted_message.length)>telegram_message_length_limit){
+        send_message_to_telegram(config.bot_token, config.chat_id, message_to_be_sent)
+        message_to_be_sent='';
+      }
+      message_to_be_sent+=formatted_message;
     }
-    const lastMessage = debBucket[debBucket.length-1];
-    if(lastMessage && lastMessage.msg ===msg ){
-      lastMessage.count+=1;
+    if(message_to_be_sent.length){
+      send_message_to_telegram(config.bot_token, config.chat_id, message_to_be_sent)
+    }
+  };
+
+  self.add_to_queue=(msg) => {
+    if(!deb_int_handler){
+      console.log('DebounceEngine is not running, invoking startup to get it up and running');
+      self.start_up()
+    }
+    const last_message = deb_bucket[deb_bucket.length-1];
+    if(last_message && last_message.msg ===msg ){
+      last_message.count+=1;
+      last_message.timestamp=(new Date()).toString()
     } else {
-      msg+=`\n-${(new Date()).toString()}`;
-      debBucket.push({msg, count:0})
+      deb_bucket.push({msg, count:0, timestamp:(new Date()).toString()})
     }
   }
-  self.startUp=() => {
-    if(!debIntHandler){
-      debIntHandler=setInterval(flushDebBucket,debounceTime);
-      console.log(`Running DebounceEngine with a debounce of ${debounceTime} milliseconds`);
+  self.start_up=() => {
+    if(!deb_int_handler){
+      deb_int_handler=setInterval(flush_deb_bucket,debounce_time);
+      console.log(`Running DebounceEngine with a debounce of ${debounce_time} milliseconds`);
     } else {
       console.log('DebounceEngine is already running')
     }
   }
-  self.shutDown=() => {
-    debIntHandler && clearInterval(debIntHandler);
-    debIntHandler=null;
-    flushDebBucket(); 
+  self.shut_down=() => {
+    deb_int_handler && clearInterval(deb_int_handler);
+    deb_int_handler=null;
+    flush_deb_bucket(); 
   }
-  self.isDebounceNeeded=() => {
-    const currentDateTime=(new Date()).getTime();
-    if(eventTraffic.lastMessageTimeStamp && (currentDateTime - eventTraffic.lastMessageTimeStamp)<=violationTime){
-      eventTraffic.consistentViolationCount++;
+  self.is_debounce_needed=() => {
+    const current_date_time=(new Date()).getTime();
+    if(event_traffic.last_message_time_stamp && (current_date_time - event_traffic.last_message_time_stamp)<=violation_time){
+      event_traffic.consistent_violation_count++;
     }else {
-      eventTraffic.consistentViolationCount=0;
+      event_traffic.consistent_violation_count=0;
     }
-    eventTraffic.lastMessageTimeStamp=currentDateTime;
-    return eventTraffic.consistentViolationCount >= violationCount
+    event_traffic.last_message_time_stamp=current_date_time;
+    return event_traffic.consistent_violation_count >= violation_count
   }
 
   init();
@@ -113,18 +128,18 @@ const Messenger = (config) => {
   const self = this;
   console.log('Initializing pm2-telegram-notification')
   
-  const debounceEngine = new DebounceEngine(config);
+  const debounce_engine = new DebounceEngine(config);
   // console.log('messenger-config', config)
   const send = (data) => {
     const msg = messageFormatter(data, config.server_name);
-    self.messaging_mode = debounceEngine.isDebounceNeeded()? messagingModes.collecting:messagingModes.instant;
-    console.log('-----------========########========------------>>>>>Messaging Mode='+self.messaging_mode);
+    self.messaging_mode = debounce_engine.is_debounce_needed()? messagingModes.collecting:messagingModes.instant;
+    console.log('Messaging Mode set as='+self.messaging_mode);
     if (self.messaging_mode ==  messagingModes.instant) {
       // console.log('TRIGGERED->message=>', message);
       // console.log('TRIGGERED->pm2_event=>', pm2_event);
       send_message_to_telegram(config.bot_token, config.chat_id, msg)
     } else {
-      debounceEngine.addToQueue(msg);
+      debounce_engine.add_to_queue(msg);
     }
   };
   return { send }
